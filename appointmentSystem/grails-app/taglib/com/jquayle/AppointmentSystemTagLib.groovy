0b7a81e04c52d1f89598a3a83bd3bd439b23da40
JosephQuayle <josephquayle@hotmail.com>
2022-04-04 15:50:34 +0000

    small layout changes

--- appointmentSystem/grails-app/taglib/com/jquayle/AppointmentSystemTagLib.groovy
+++ appointmentSystem/grails-app/taglib/com/jquayle/AppointmentSystemTagLib.groovy
@@ -8,14 +8,14 @@ class AppointmentSystemTagLib {
 	
 	out << "<div style ='margin-left:0px 0 0px;'>"
 	if (request.getSession(false) && session.user){
-	out << "<span style=float: left; padding-left: 200px;margin-left:400px'>"
+	out << "<span style=float: left; padding-left: px;margin-left:px'>"
 	out << "Welcome ${session.user}.<br>"
-	out << "</span><span style='float:right;margin-right:200px'>"
+	out << "</span><span style='float:right;margin-right:px'>"
 	out << "<a href='${createLink(controller:'Receptionist', action:'logout')}'<button type='button' class='btn link'>"
 	out << "Logout </button></a></span>"
 	}else{
 	
-	out << "<span style='float:right;margin-right:200px'>"
+	out << "<span style='float:right;margin-right:px'>"
 	out << "<a href='${createLink(controller:'Receptionist', action:'login')}'<button type='button' class='btn link'>"
 	out << "Receptionist Login </button> </a></span>"
 	}
@@ -26,16 +26,16 @@ class AppointmentSystemTagLib {
 
 	def loginToggleMainDoc={
 	
-	out << "<div style ='margin-left:200px 0 0px;'>"
+	out << "<div style ='margin-left:px 0 0px;'>"
 	if (request.getSession(false) && session.user1){
-	out << "<span style=float: left; padding-left: 200px;margin-left:400px'>"
+	out << "<span style=float: left; padding-left: px;margin-left:px'>"
 	out << "Welcome ${session.user1}.<br>"
-	out << "</span><span style='float:right;margin-right:200px'>"
+	out << "</span><span style='float:right;margin-right:px'>"
 	out << "<a href='${createLink(controller:'Doctor', action:'logout')}'<button <button type='button' class='btn link'>"
 	out << "Logout </button></a></span>"
 	}else{
 	
-	out << "<span style='float:right;margin-right:200px'>"
+	out << "<span style='float:right;margin-right:px'>"
 	out << "<a href='${createLink(controller:'Doctor', action:'login')}'<button type='button' class='btn link'>"
 	out << "Doctor Login </button></a></span>"
 	}
